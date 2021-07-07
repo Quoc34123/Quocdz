eac29effa2836ad37fe9f3d04e4f44f2bccef190
Quoc34123 <86888070+Quoc34123@users.noreply.github.com>
2021-07-07 12:46:02 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "Hello Huy!",
+  text1: "He luu Hân!",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Huy nghỉ Quốc là người như thế nào ? ._.",
-  text4: "Ai thoát ra làm chó:v",
-  text5: "Xàm lồn",
-  text6: "Đẹp trai hiền lành tốt bụng <3",
-  text7: "Vậy Huy có muốn làm bạn với Quốc ko??? :vvvv",
+  text3: "Cậu có yêu tớ ko nào? ._.",
+  text4: "Nếu cậu ko trả lời mà thoát ra tức là :v",
+  text5: "Cậu mơ à",
+  text6: "Yêu ơi là yêu<3",
+  text7: "Nói cho tớ lí do cậu thích tớ đi:vvvv",
   text8: "Gửi cho tớ <3",
-  text9: "ko huy muốn làm ngiu của Quốc cơ <3",
+  text9: "Vì cậu đẹp try vllll",
   text10: "Tớ biết mà ^^ Yêu cậu 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Tối nay tớ với cậu cùng chơi game nhé :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
   text12: "Okii lunn <3",
 };
 
